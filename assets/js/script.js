2ac86062aec3a602ca3b2834ea2d55478e5a2132
maahmp1 <marcelamartinspena@gmail.com>
2024-06-01 16:19:57 +1200

    website portfolio

---
+++ assets/js/script.js
@@ -0,0 +1,74 @@
+function documentReady(fn) {
+    if (document.readyState != 'loading') {
+      return fn()
+    }
+  
+    return document.addEventListener('DOMContentLoaded', fn)
+  }
+  
+  var toggleClass = function (selector, value) {
+    if (selector.classList.contains(value)) {
+      selector.classList.remove(value)
+    } else {
+      selector.classList.add(value)
+    }
+  }
+  
+  var toggleSidebar = function () {
+    Array.prototype.forEach.call(
+      document.getElementsByClassName('button'),
+      function (element, i) {
+        toggleClass(element, 'active')
+      }
+    )
+    toggleClass(document.querySelector('main'), 'move-to-left')
+    Array.prototype.forEach.call(
+      document.getElementsByClassName('sidebar-item'),
+      function (element, i) {
+        toggleClass(element, 'active')
+      }
+    )
+    Array.prototype.forEach.call(
+      document.getElementsByClassName('detail'),
+      function (element, i) {
+        toggleClass(element, 'move-info-to-right')
+      }
+    )
+  }
+  
+  documentReady(function () {
+    Array.prototype.forEach.call(
+      document.getElementsByClassName('button'),
+      function (element, i) {
+        element.addEventListener('click', function () {
+          toggleSidebar()
+        })
+        element.addEventListener('tap', function () {
+          toggleSidebar()
+        })
+      }
+    )
+  
+    document.addEventListener('keyup', function (e) {
+      if (e.keyCode === 27) {
+        toggleSidebar()
+      }
+    })
+  
+    if ('IntersectionObserver' in window) {
+      function onIntersection(images, observer) {
+        images.forEach(image => {
+          observer.observe(image)
+        })
+      }
+  
+      var images = document.querySelectorAll('img')
+      var config = {
+        rootMargin: '50px 0px',
+        threshold: 0.01
+      }
+  
+      var observer = new IntersectionObserver(onIntersection, config)
+    }
+  })
+  
